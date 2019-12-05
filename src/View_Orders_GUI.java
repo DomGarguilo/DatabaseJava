b3242db3681bb8231d38abd25440594bdddc6f9d
mrana1 <32280437+mrana1@users.noreply.github.com>
2019-12-05 00:28:53 -0500

    Update View_Orders_GUI.java

--- src/View_Orders_GUI.java
+++ src/View_Orders_GUI.java
@@ -18,10 +18,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.JTextField;
 public class View_Orders_GUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -45,7 +48,7 @@ public class View_Orders_GUI extends JFrame {
 	public View_Orders_GUI() {
 		setBackground(new Color(255, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 900, 720);
+		setBounds(100, 100, 900, 958);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(128, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -75,7 +78,7 @@ public class View_Orders_GUI extends JFrame {
 		contentPane.add(btnBack);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(62, 228, 742, 399);
+		scrollPane.setBounds(62, 228, 742, 264);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
@@ -87,6 +90,52 @@ public class View_Orders_GUI extends JFrame {
 			}
 		));
 		scrollPane.setViewportView(table);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(62, 608, 742, 264);
+		contentPane.add(scrollPane_1);
+		
+		JLabel lblPastaOrders = new JLabel("Pasta Orders:");
+		lblPastaOrders.setForeground(Color.WHITE);
+		lblPastaOrders.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblPastaOrders.setBounds(62, 171, 162, 41);
+		contentPane.add(lblPastaOrders);
+		
+		JLabel lblBreakfastOrders = new JLabel("Breakfast Orders:");
+		lblBreakfastOrders.setForeground(Color.WHITE);
+		lblBreakfastOrders.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblBreakfastOrders.setBounds(62, 554, 209, 41);
+		contentPane.add(lblBreakfastOrders);
+		
+		JButton btnCompleteBreakfastOrder = new JButton("Complete Breakfast Order");
+		btnCompleteBreakfastOrder.setBounds(623, 567, 181, 25);
+		contentPane.add(btnCompleteBreakfastOrder);
+		
+		JButton btnCompletePastaOrder = new JButton("Complete Pasta Order");
+		btnCompletePastaOrder.setBounds(623, 184, 181, 25);
+		contentPane.add(btnCompletePastaOrder);
+		
+		textField = new JTextField();
+		textField.setBounds(494, 185, 116, 22);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(495, 568, 116, 22);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblEnterOrderNumber = new JLabel("Enter Order Number:");
+		lblEnterOrderNumber.setForeground(Color.WHITE);
+		lblEnterOrderNumber.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEnterOrderNumber.setBounds(317, 174, 165, 41);
+		contentPane.add(lblEnterOrderNumber);
+		
+		JLabel label = new JLabel("Enter Order Number:");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		label.setBounds(317, 557, 165, 41);
+		contentPane.add(label);
 		
 		DefaultTableModel model = (DefaultTableModel) table.getModel();//Model here
 
